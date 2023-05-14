a0f5ddd15858cfa3d01d1ab377ff1786ef5dbd6e
biiit4894 <ghiesf@naver.com>
2023-05-15 02:30:50 +0900

    [ADD] 점주 회원가입 DTO 생성

---
+++ src/interfaces/user/ownerCreateDTO.ts
@@ -0,0 +1,15 @@
+export interface OwnerCreateDTO {
+  loginId: string;
+  password: string;
+  store: string;
+  director: string;
+  phone: string;
+  email: string;
+  address: string;
+  detailAddress: string;
+  licenseNumber: string;
+  licenseImage: string;
+  authorized: boolean;
+  termsAgree: boolean;
+  marketingAgree: boolean;
+}
