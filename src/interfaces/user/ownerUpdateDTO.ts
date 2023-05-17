cc4d3a51c9943d2832e919c1d5f3faff423829c9
biiit4894 <ghiesf@naver.com>
2023-05-17 19:03:01 +0900

    [ADD] 점주 회원정보 수정 DTO 생성

---
+++ src/interfaces/user/ownerUpdateDTO.ts
@@ -0,0 +1,10 @@
+export interface OwnerUpdateDTO {
+  password: string;
+  director: string;
+  phone: string;
+  email: string;
+  address: string;
+  detailAddress: string;
+  licenseNumber: string;
+  licenseImage: string;
+}
