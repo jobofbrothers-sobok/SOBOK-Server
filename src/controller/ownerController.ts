809116ac19b5bc79ebf199adc1a15bd7d649049e
biiit4894 <ghiesf@naver.com>
2023-06-26 02:34:06 +0900

    [CHORE] 매장 메뉴 등록 시 이미지 등록 가능하도록 수정

--- src/controller/ownerController.ts
+++ src/controller/ownerController.ts
@@ -151,16 +151,24 @@ const createStoreNotice = async (req: Request, res: Response) => {
 
 // 점주 매장메뉴 등록
 const createStoreMenu = async (req: Request, res: Response) => {
+  const error = validationResult(req);
+  if (!error.isEmpty()) {
+    return res
+      .status(sc.BAD_REQUEST)
+      .send(fail(sc.BAD_REQUEST, rm.BAD_REQUEST));
+  }
   const createStoreMenuDTO: CreateStoreMenuDTO = req.body;
-  const userId = req.user.id;
-  const storeId = await ownerService.getStorebyOwnerId(userId);
-  console.log(storeId);
+
+  const image: Express.Multer.File = req.file as Express.Multer.File;
+  const path = image.path;
+
+  const storeId = req.params.id;
   try {
     const createdMenu = await ownerService.createStoreMenu(
       createStoreMenuDTO,
-      storeId as number
+      path,
+      +storeId
     );
-    console.log(storeId);
     return res
       .status(sc.OK)
       .send(success(sc.OK, rm.CREATE_STORE_MENU_SUCCESS, createdMenu));
