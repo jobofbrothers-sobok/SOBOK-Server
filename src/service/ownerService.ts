7e0676cca2c66637ea54e1a5d36e7efbd1faa805
biiit4894 <ghiesf@naver.com>
2023-06-26 01:56:51 +0900

    [FIX] 점주 매장정보 등록/수정 시 이미지 등록 가능하도록 수정

--- src/service/ownerService.ts
+++ src/service/ownerService.ts
@@ -11,54 +11,6 @@ import { CreateStoreMenuDTO } from "../interfaces/store/createStoreMenuDTO";
 
 const prisma = new PrismaClient();
 
-// 점주 유저 생성
-const createOwner = async (ownerCreateDTO: OwnerCreateDTO) => {
-  // 넘겨받은 password를 bcrypt의 도움을 받아 암호화
-  const salt = await bcrypt.genSalt(10); // 매우 작은 임의의 랜덤 텍스트 salt
-  const password = await bcrypt.hash(ownerCreateDTO.password, salt); // 위에서 랜덤을 생성한 salt를 이용해 암호화
-  const data = await prisma.store_Owner.create({
-    data: {
-      loginId: ownerCreateDTO.loginId,
-      password,
-      store: ownerCreateDTO.store,
-      director: ownerCreateDTO.director,
-      phone: ownerCreateDTO.phone,
-      email: ownerCreateDTO.email,
-      address: ownerCreateDTO.address,
-      detailAddress: ownerCreateDTO.detailAddress,
-      licenseNumber: ownerCreateDTO.licenseNumber,
-      licenseImage: ownerCreateDTO.licenseImage,
-      authorized: ownerCreateDTO.authorized,
-      termsAgree: ownerCreateDTO.termsAgree,
-      marketingAgree: ownerCreateDTO.marketingAgree,
-    },
-  });
-
-  return data;
-};
-
-// 점주 유저 로그인
-const ownerSignIn = async (userSignInDTO: UserSignInDTO) => {
-  try {
-    const user = await prisma.store_Owner.findFirst({
-      where: {
-        loginId: userSignInDTO.loginId,
-      },
-    });
-    if (!user) return null;
-
-    // bcrypt가 DB에 저장된 기존 password와 넘겨 받은 password를 대조하고
-    // match false시 401을 리턴
-    const isMatch = await bcrypt.compare(userSignInDTO.password, user.password);
-    if (!isMatch) return sc.UNAUTHORIZED;
-
-    return user.id;
-  } catch (error) {
-    console.log(error);
-    throw error;
-  }
-};
-
 // 점주 유저 회원정보 수정
 const updateOwner = async (id: number, ownerUpdateDTO: OwnerUpdateDTO) => {
   // 넘겨받은 password를 bcrypt의 도움을 받아 암호화
@@ -77,16 +29,6 @@ const updateOwner = async (id: number, ownerUpdateDTO: OwnerUpdateDTO) => {
       detailAddress: ownerUpdateDTO.detailAddress,
       licenseNumber: ownerUpdateDTO.licenseNumber,
       licenseImage: ownerUpdateDTO.licenseImage,
-    },
-  });
-  return data.id;
-};
-
-// 점주 유저 회원탈퇴
-const ownerDelete = async (id: number) => {
-  const data = await prisma.store_Owner.delete({
-    where: {
-      id: id,
     },
   });
   return data.id;
@@ -115,7 +57,8 @@ const findOwnerById = async (id: number) => {
 // 점주 매장정보 등록
 const createStoreInfo = async (
   createStoreInfoDTO: CreateStoreInfoDTO,
-  ownerId: number
+  ownerId: number,
+  path: string
 ) => {
   const data = await prisma.store.create({
     data: {
@@ -124,7 +67,7 @@ const createStoreInfo = async (
       officeHour: createStoreInfoDTO.officeHour,
       dayOff: createStoreInfoDTO.dayOff,
       homepage: createStoreInfoDTO.homepage,
-      image: createStoreInfoDTO.image,
+      image: path,
       category: createStoreInfoDTO.category,
       ownerId,
     },
@@ -148,7 +91,8 @@ const createStoreIdForOwner = async (ownerId: number, storeId: number) => {
 // 점주 매장정보 수정
 const updateStoreInfo = async (
   storeId: number,
-  createStoreInfoDTO: CreateStoreInfoDTO
+  createStoreInfoDTO: CreateStoreInfoDTO,
+  path: string
 ) => {
   const data = await prisma.store.update({
     where: {
@@ -160,7 +104,7 @@ const updateStoreInfo = async (
       officeHour: createStoreInfoDTO.officeHour,
       dayOff: createStoreInfoDTO.dayOff,
       homepage: createStoreInfoDTO.homepage,
-      image: createStoreInfoDTO.image,
+      image: path,
       category: createStoreInfoDTO.category,
     },
   });
@@ -284,10 +228,7 @@ const grantStampByRandNum = async (
 };
 
 const ownerService = {
-  createOwner,
-  ownerSignIn,
   updateOwner,
-  ownerDelete,
   getOwnerName,
   findOwnerById,
   createStoreInfo,
