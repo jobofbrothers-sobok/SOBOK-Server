44d5f4b7f572ed189ed8f3881d6e750bd1ada77a
biiit4894 <ghiesf@naver.com>
2023-06-26 01:56:51 +0900

    [FIX] 점주 매장정보 등록/수정 시 이미지 등록 가능하도록 수정

--- src/controller/ownerController.ts
+++ src/controller/ownerController.ts
@@ -13,92 +13,6 @@ import { OwnerUpdateDTO } from "../interfaces/user/ownerUpdateDTO";
 import { CreateStoreNoticeDTO } from "../interfaces/store/createStoreNoticeDTO";
 import { customerService } from "../service";
 
-// 점주 유저 생성
-const createOwner = async (req: Request, res: Response) => {
-  // validation의 결과를 바탕으로 분기 처리
-  const error = validationResult(req);
-  if (!error.isEmpty()) {
-    console.log(error);
-    return res
-      .status(sc.BAD_REQUEST)
-      .send(fail(sc.BAD_REQUEST, rm.BAD_REQUEST));
-  }
-
-  const ownerCreateDTO: OwnerCreateDTO = req.body;
-  const termsAgree = req.body.termsAgree;
-
-  // if (!termsAgree) {
-  //   return res.status(sc.BAD_REQUEST).send(fail(sc.BAD_REQUEST, rm.NULL_VALUE));
-  // }
-
-  try {
-    const data = await ownerService.createOwner(ownerCreateDTO);
-
-    if (!data) {
-      return res
-        .status(sc.BAD_REQUEST)
-        .send(fail(sc.BAD_REQUEST, rm.SIGNUP_FAIL));
-    }
-
-    // jwtHandler 내 sign 함수를 이용해 accessToken 생성
-    const accessToken = jwtHandler.sign(data.id);
-
-    const result = {
-      userId: data.id,
-      name: data.store,
-      accessToken,
-    };
-
-    return res
-      .status(sc.CREATED)
-      .send(success(sc.CREATED, rm.SIGNUP_SUCCESS, result));
-  } catch (e) {
-    console.log(e);
-    // 서버 내부에서 오류 발생
-    res
-      .status(sc.INTERNAL_SERVER_ERROR)
-      .send(fail(sc.INTERNAL_SERVER_ERROR, rm.INTERNAL_SERVER_ERROR));
-  }
-};
-
-// 점주 유저 로그인
-const ownerSignIn = async (req: Request, res: Response) => {
-  const error = validationResult(req);
-  if (!error.isEmpty()) {
-    return res
-      .status(sc.BAD_REQUEST)
-      .send(fail(sc.BAD_REQUEST, rm.BAD_REQUEST));
-  }
-
-  const userSigninDTO: UserSignInDTO = req.body;
-
-  try {
-    const userId = await ownerService.ownerSignIn(userSigninDTO);
-
-    if (!userId)
-      return res.status(sc.NOT_FOUND).send(fail(sc.NOT_FOUND, rm.NOT_FOUND));
-    else if (userId === sc.UNAUTHORIZED)
-      return res
-        .status(sc.UNAUTHORIZED)
-        .send(fail(sc.UNAUTHORIZED, rm.INVALID_PASSWORD));
-
-    const accessToken = jwtHandler.sign(userId);
-
-    const result = {
-      userId: userId,
-      accessToken,
-    };
-
-    res.status(sc.OK).send(success(sc.OK, rm.SIGNIN_SUCCESS, result));
-  } catch (e) {
-    console.log(error);
-    // 서버 내부에서 오류 발생
-    res
-      .status(sc.INTERNAL_SERVER_ERROR)
-      .send(fail(sc.INTERNAL_SERVER_ERROR, rm.INTERNAL_SERVER_ERROR));
-  }
-};
-
 // 점주 유저 회원정보 수정
 const updateOwner = async (req: Request, res: Response) => {
   const error = validationResult(req);
@@ -115,23 +29,6 @@ const updateOwner = async (req: Request, res: Response) => {
     return res
       .status(sc.OK)
       .send(success(sc.OK, rm.UPDATE_USER_SUCCESS, { id: updatedUserId }));
-  } catch (error) {
-    console.log(error);
-    return res
-      .status(sc.INTERNAL_SERVER_ERROR)
-      .send(fail(sc.INTERNAL_SERVER_ERROR, rm.INTERNAL_SERVER_ERROR));
-  }
-};
-
-// 점주 유저 회원탈퇴
-const ownerDelete = async (req: Request, res: Response) => {
-  try {
-    const id = req.user.id;
-    const deletedUserId = await ownerService.ownerDelete(+id);
-
-    return res
-      .status(sc.OK)
-      .send(success(sc.OK, rm.DELETE_USER_SUCCESS, { id: deletedUserId }));
   } catch (error) {
     console.log(error);
     return res
@@ -161,11 +58,16 @@ const getOwnerName = async (req: Request, res: Response) => {
 const createStoreInfo = async (req: Request, res: Response) => {
   const createStoreInfoDTO: CreateStoreInfoDTO = req.body;
   const ownerId = req.user.id;
+  const image: Express.Multer.File = req.file as Express.Multer.File;
+  const path = image.path;
   try {
     const createStore = await ownerService.createStoreInfo(
       createStoreInfoDTO,
-      ownerId
+      ownerId,
+      path
     );
+    // console.log(typeof createStore.description, createStore.description);
+    // console.log(typeof createStore.category, createStore.category);
     const storeId = createStore.id;
     const createStoreIdForOwner = await ownerService.createStoreIdForOwner(
       ownerId,
@@ -189,11 +91,16 @@ const updateStoreInfo = async (req: Request, res: Response) => {
   const createStoreInfoDTO: CreateStoreInfoDTO = req.body;
   // const userId = req.user.id;
   const storeId = req.params.id;
+  const image: Express.Multer.File = req.file as Express.Multer.File;
+  const path = image.path;
   try {
     const updatedStore = await ownerService.updateStoreInfo(
       +storeId,
-      createStoreInfoDTO
+      createStoreInfoDTO,
+      path
     );
+    // console.log(typeof updatedStore.description, updatedStore.description);
+    // console.log(typeof updatedStore.category, updatedStore.category);
     return res
       .status(sc.OK)
       .send(success(sc.OK, rm.UPDATE_STORE_INFO_SUCCESS, updatedStore));
@@ -309,10 +216,7 @@ const grantStampByRandNum = async (req: Request, res: Response) => {
 };
 
 const ownerController = {
-  createOwner,
-  ownerSignIn,
   updateOwner,
-  ownerDelete,
   getOwnerName,
   createStoreInfo,
   updateStoreInfo,
--- src/router/ownerRouter.ts
+++ src/router/ownerRouter.ts
@@ -2,8 +2,10 @@ import { Router } from "express";
 import { ownerController } from "../controller";
 import { body } from "express-validator";
 import { auth } from "../middlewares";
+import multer from "multer";
 
 const router: Router = Router();
+const storeUpload = multer({ dest: "uploads/owner/store/" });
 
 // 점주 매장소식 등록 - POST ~/owner/store/notice
 router.post("/store/notice", auth, ownerController.createStoreNotice);
@@ -15,36 +17,26 @@ router.post("/store/menu", auth, ownerController.createStoreMenu);
 router.post("/store/product", auth, ownerController.createStoreProduct);
 
 // 점주 매장정보 수정 - POST ~/owner/store/info/:id
-router.post("/store/:id", auth, ownerController.updateStoreInfo);
+router.post(
+  "/store/:id",
+  auth,
+  storeUpload.single("file"),
+  ownerController.updateStoreInfo
+);
 
 // 점주 매장정보 등록 - POST ~/owner/store/info
-router.post("/store", auth, ownerController.createStoreInfo);
+router.post(
+  "/store",
+  auth,
+  storeUpload.single("file"),
+  ownerController.createStoreInfo
+);
 
 // 점주 스탬프 적립 - POST ~/owner/stamp
 router.post("/stamp", auth, ownerController.grantStampByRandNum);
 
 // // 점주 스탬프 서비스 사용 신청 - POST ~/owner/request
 // router.post("/request", auth, ownerController.requestStampSignIn);
-
-// 점주 유저 생성 - POST ~/owner/signup
-router.post(
-  "/signup",
-  [
-    body("loginId").trim().notEmpty(),
-    body("password").trim().notEmpty(),
-    body("store").trim().notEmpty(),
-    body("director").trim().notEmpty(),
-    body("phone").trim().notEmpty(),
-    body("email").trim().notEmpty(),
-    body("address").trim().notEmpty(),
-    body("licenseNumber").trim().notEmpty(),
-    body("licenseImage").trim().notEmpty(),
-  ],
-  ownerController.createOwner
-);
-
-// 점주 유저 로그인 - POST ~/owner/signin
-router.post("/signin", ownerController.ownerSignIn);
 
 // 점주 유저 회원정보 수정 - POST ~/owner/:id
 router.post(
@@ -61,9 +53,6 @@ router.post(
   ],
   ownerController.updateOwner
 );
-
-// 점주 유저 회원탈퇴 - DELETE ~/owner
-router.delete("/", auth, ownerController.ownerDelete);
 
 // 점주 유저 이름 조회 - GET ~/owner/:id
 router.get("/:id", ownerController.getOwnerName);
