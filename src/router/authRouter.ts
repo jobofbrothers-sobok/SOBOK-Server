fb4dc447ee44d1536f61087ab04818e2d183c74c
biiit4894 <ghiesf@naver.com>
2023-06-25 14:21:02 +0900

    [FIX] 담당자 정보 수정 시 프로필 사진, 사업자 등록증 이미지 모두 수정 가능하도록

--- src/router/authRouter.ts
+++ src/router/authRouter.ts
@@ -55,17 +55,17 @@ router.post(
 router.post(
   "/update/owner",
   auth,
-  ownerUpload.single("file"),
-  [
-    body("password").trim().notEmpty(),
-    body("director").trim().notEmpty(),
-    body("phone").trim().notEmpty(),
-    body("email").trim().notEmpty(),
-    body("address").trim().notEmpty(),
-    body("detailAddress").trim().notEmpty(),
-    body("licenseNumber").trim().notEmpty(),
-    body("licenseImage").trim().notEmpty(),
-  ],
+  ownerUpload.fields([{ name: "file1" }, { name: "file2" }]),
+  // [
+  //   body("password").trim().notEmpty(),
+  //   body("director").trim().notEmpty(),
+  //   body("phone").trim().notEmpty(),
+  //   body("email").trim().notEmpty(),
+  //   body("address").trim().notEmpty(),
+  //   body("detailAddress").trim().notEmpty(),
+  //   body("licenseNumber").trim().notEmpty(),
+  //   body("licenseImage").trim().notEmpty(),
+  // ],
   authController.ownerUpdate
 );
 
--- src/service/customerService.ts
+++ src/service/customerService.ts
@@ -9,82 +9,6 @@ import ownerService from "./ownerService";
 
 const prisma = new PrismaClient();
 
-// 고객 유저 생성
-const createCustomer = async (customerCreateDTO: CustomerCreateDTO) => {
-  // 넘겨받은 password를 bcrypt의 도움을 받아 암호화
-  const salt = await bcrypt.genSalt(10); // 매우 작은 임의의 랜덤 텍스트 salt
-  const password = await bcrypt.hash(customerCreateDTO.password, salt); // 위에서 랜덤으로 생성한 salt를 이용해 암호화
-  const data = await prisma.customer.create({
-    data: {
-      loginId: customerCreateDTO.loginId,
-      password,
-      name: customerCreateDTO.name,
-      email: customerCreateDTO.email,
-      phone: customerCreateDTO.phone,
-      termsAgree: customerCreateDTO.termsAgree,
-      marketingAgree: customerCreateDTO.marketingAgree,
-    },
-  });
-
-  return data;
-};
-
-// 고객 유저 로그인
-const customerSignIn = async (userSignInDTO: UserSignInDTO) => {
-  try {
-    const user = await prisma.customer.findFirst({
-      where: {
-        loginId: userSignInDTO.loginId,
-      },
-    });
-    if (!user) return null;
-
-    // bcrypt가 DB에 저장된 기존 password와 넘겨 받은 password를 대조하고
-    // match false시 401을 리턴
-    const isMatch = await bcrypt.compare(userSignInDTO.password, user.password);
-    if (!isMatch) return sc.UNAUTHORIZED;
-
-    return user.id;
-  } catch (error) {
-    console.log(error);
-    throw error;
-  }
-};
-
-// 고객 유저 회원정보 수정
-const updateCustomer = async (
-  id: number,
-  customerUpdateDTO: CustomerUpdateDTO,
-  path: string
-) => {
-  // 넘겨받은 password를 bcrypt의 도움을 받아 암호화
-  const salt = await bcrypt.genSalt(10);
-  const password = await bcrypt.hash(customerUpdateDTO.password, salt); // 위에서 랜덤으로 생성한 salt를 이용해 암호화
-  const data = await prisma.customer.update({
-    where: {
-      id,
-    },
-    data: {
-      password: password,
-      name: customerUpdateDTO.name,
-      email: customerUpdateDTO.email,
-      phone: customerUpdateDTO.phone,
-      image: path,
-    },
-  });
-  return data.id;
-};
-
-// 고객 유저 회원탈퇴
-const customerDelete = async (id: number) => {
-  const data = await prisma.customer.delete({
-    where: {
-      id: id,
-    },
-  });
-  return data.id;
-};
-
 // 고객 유저 이름 조회
 const getCustomerName = async (id: number) => {
   const data = await prisma.customer.findUnique({
@@ -229,10 +153,6 @@ const createDeliveryRequest = async (
 };
 
 const customerService = {
-  createCustomer,
-  customerSignIn,
-  updateCustomer,
-  customerDelete,
   getCustomerName,
   findCustomerById,
   createStampNumber,
