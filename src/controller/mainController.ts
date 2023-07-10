384509d672316ae7f161a4cff126355bb3a685fe
biiit4894 <ghiesf@naver.com>
2023-07-11 06:32:36 +0900

    [CHORE] 근처 카페 전체 조회 라우팅 해제(임시)

--- src/controller/mainController.ts
+++ src/controller/mainController.ts
@@ -62,29 +62,29 @@ const deleteLikeCafe = async (req: Request, res: Response) => {
 };
 
 // 유저 근처 카페 전체 조회
-const getAllCafe = async (req: Request, res: Response) => {
-  // 유저 현위치 x, y 좌표
-  const x = req.body.x;
-  const y = req.body.y;
-  const category = req.body.category;
+// const getAllCafe = async (req: Request, res: Response) => {
+//   // 유저 현위치 x, y 좌표
+//   const x = req.body.x;
+//   const y = req.body.y;
+//   const category = req.body.category;
 
-  try {
-    const data = await mainService.getAllCafe(x, y, category);
-    if (!data) {
-      return res
-        .status(sc.BAD_REQUEST)
-        .send(fail(sc.BAD_REQUEST, rm.GET_ALL_NEAR_CAFE_FAIL));
-    }
-    return res
-      .status(sc.OK)
-      .send(success(sc.OK, rm.GET_ALL_NEAR_CAFE_SUCCESS, data));
-  } catch (error) {
-    console.log(error);
-    res
-      .status(sc.INTERNAL_SERVER_ERROR)
-      .send(fail(sc.INTERNAL_SERVER_ERROR, rm.INTERNAL_SERVER_ERROR));
-  }
-};
+//   try {
+//     const data = await mainService.getAllCafe(x, y, category);
+//     if (!data) {
+//       return res
+//         .status(sc.BAD_REQUEST)
+//         .send(fail(sc.BAD_REQUEST, rm.GET_ALL_NEAR_CAFE_FAIL));
+//     }
+//     return res
+//       .status(sc.OK)
+//       .send(success(sc.OK, rm.GET_ALL_NEAR_CAFE_SUCCESS, data));
+//   } catch (error) {
+//     console.log(error);
+//     res
+//       .status(sc.INTERNAL_SERVER_ERROR)
+//       .send(fail(sc.INTERNAL_SERVER_ERROR, rm.INTERNAL_SERVER_ERROR));
+//   }
+// };
 
 // 유저 근처 카페 개별 업체 정보 조회
 const getCafeById = async (req: Request, res: Response) => {
@@ -304,7 +304,7 @@ const getCustomerMyPage = async (req: Request, res: Response) => {
 const mainController = {
   createLikeCafe,
   deleteLikeCafe,
-  getAllCafe,
+  //getAllCafe,
   getCafeById,
   getCafeNoticeById,
   getCafeMenuById,
