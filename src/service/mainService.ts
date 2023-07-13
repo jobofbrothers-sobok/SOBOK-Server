9c4a76c3dc7ba0ed01dbc7c9011926ecfedbf0b2
biiit4894 <ghiesf@naver.com>
2023-07-13 18:04:47 +0900

    [FIX] request body 수정

--- src/service/mainService.ts
+++ src/service/mainService.ts
@@ -7,7 +7,7 @@ import axios from "axios";
 const prisma = new PrismaClient();
 
 // 키워드로 카페 검색
-const getCafeByKeyword = async (keyword: string, customerId: number) => {
+const getCafeByKeyword = async (keyword: string) => {
   const data = await prisma.store.findMany({
     where: {
       OR: [
