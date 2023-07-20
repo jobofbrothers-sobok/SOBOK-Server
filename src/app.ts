fefc97ced538723778115db3de060e2f3ada911a
biiit4894 <ghiesf@naver.com>
2023-07-20 13:56:23 +0900

    [ADD] 정적 파일 제공 주소 추가

--- src/app.ts
+++ src/app.ts
@@ -15,8 +15,40 @@ app.use(
 );
 
 // 이미지 폴더 접근 권한 허용
-app.use("/uploads", express.static("/uploads"));
-console.log("*****__dirname: ", __dirname);
+app.use(
+  "/uploads/owner/store/menu",
+  express.static(__dirname + "/uploads/owner/store/menu")
+);
+app.use(
+  "/uploads/owner/store/notice",
+  express.static(__dirname + "/uploads/owner/store/notice")
+);
+app.use(
+  "/uploads/owner/store/product",
+  express.static(__dirname + "/uploads/owner/store/product")
+);
+app.use(
+  "/uploads/owner/store",
+  express.static(__dirname + "/uploads/owner/store")
+);
+app.use(
+  "/uploads/customer/review",
+  express.static(__dirname + "/uploads/customer/review")
+);
+app.use(
+  "/uploads/manager/notice",
+  express.static(__dirname + "/uploads/manager/notice")
+);
+app.use(
+  "/uploads/manager/tour",
+  express.static(__dirname + "/uploads/manager/tour")
+);
+
+app.use("/uploads/customer", express.static(__dirname + "/uploads/customer"));
+app.use("/uploads/owner", express.static(__dirname + "/uploads/owner"));
+app.use("/uploads/manager", express.static(__dirname + "/uploads/manager"));
+
+console.log("*****__dirname: ", __dirname + app.resource);
 // /uploads/owner
 // /uploads/customer
 
