491d2b161af72491d6cdc0b2a97ed47e869d62be
biiit4894 <ghiesf@naver.com>
2023-07-31 15:01:03 +0900

    [CHORE] express.static 경로 수정

--- src/app.ts
+++ src/app.ts
@@ -22,30 +22,30 @@ app.use(
 );
 
 // 이미지 폴더 접근 권한 허용
-app.use(
-  "/uploads/owner/store/menu",
-  express.static("/src/uploads/owner/store/menu")
-);
-app.use(
-  "/uploads/owner/store/notice",
-  express.static("/src/uploads/owner/store/notice")
-);
-app.use(
-  "/uploads/owner/store/product",
-  express.static("/uploads/owner/store/product")
-);
-app.use(
-  "/uploads/owner/store",
-  express.static(path.join("/home/sobok/SOBOK-SERVER", "src"))
-);
+// app.use(
+//   "/uploads/owner/store/menu",
+//   express.static("/src/uploads/owner/store/menu")
+// );
+// app.use(
+//   "/uploads/owner/store/notice",
+//   express.static("/src/uploads/owner/store/notice")
+// );
+// app.use(
+//   "/uploads/owner/store/product",
+//   express.static("/uploads/owner/store/product")
+// );
+// app.use(
+//   "/uploads/owner/store",
+//   express.static(path.join("/home/sobok/SOBOK-SERVER", "src"))
+// );
 
-app.use("/uploads/customer/review", express.static("/uploads/customer/review"));
-app.use("/uploads/manager/notice", express.static("/uploads/manager/notice"));
-app.use("/uploads/manager/tour", express.static("/uploads/manager/tour"));
+// app.use("/uploads/customer/review", express.static("/uploads/customer/review"));
+// app.use("/uploads/manager/notice", express.static("/uploads/manager/notice"));
+// app.use("/uploads/manager/tour", express.static("/uploads/manager/tour"));
 
-app.use("/uploads/customer", express.static("/uploads/customer"));
-app.use("/uploads/owner", express.static(path.join(__dirname, "src")));
-app.use("/uploads/manager", express.static("/uploads/manager"));
+// app.use("/uploads/customer", express.static("/uploads/customer"));
+// app.use("/uploads/owner", express.static(path.join(__dirname, "src")));
+// app.use("/uploads/manager", express.static("/uploads/manager"));
 
 // 매장정보 등록 및 수정 시 express.static 사용
 app.use("/uploads", express.static(__dirname + "/src"));
